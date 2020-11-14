63654ccfe3365d2edfec5596161880b6c3904930
Zallyy <Zallybags@gmail.com>
2020-11-14 03:13:17 -0800

    Day 5 progress

--- 5-Sunny_with_a_chance_of_asteroids/1.js
+++ 5-Sunny_with_a_chance_of_asteroids/1.js
@@ -1,32 +1,84 @@
-// --- Day 2: 1202 Program Alarm ---
-
+// --- Day 5: Sunny with a Chance of Asteroids ---
+const prompt = require('prompt-sync')();
 const fs = require('fs')
 
-const sting_input = fs.readFileSync('./input.txt', 'UTF-8')
-const number_input = sting_input.split(",").map(num => +num) //Converts input to array of numbers
+const input = fs.readFileSync('./input.txt', 'UTF-8')
+const string_input = input.split(",").map(num => num) //Converts input to array of numbers
 
-const IntcodeComputer = (program, noun, verb) => {
-    const array = [...program]
-    array[1] = noun
-    array[2] = verb
-    for (let opcodeIndex = 0; opcodeIndex < array.length; opcodeIndex += 4) {
-        let answer = 0
-        switch(array[opcodeIndex]) {
-            case 1: //Add Numbers
-                answer = array[array[opcodeIndex+1]] + array[array[opcodeIndex+2]]
-                array[array[opcodeIndex+3]] = answer
-                break;
-            case 2: //Multiply Numbers
-                answer = array[array[opcodeIndex+1]] * array[array[opcodeIndex+2]]
-                array[array[opcodeIndex+3]] = answer
-                break;
-            case 3: //Store at address
-                answer = array[array[opcodeIndex+1]]
-                break;
-            case 99: //End Program
-                return array[0]
+const IntcodeComputer = (program) => {
+    const array = program.map(num => +num)
+    let userInput
+    let ip //Instruction pointer 
+
+    for (let i=0; i<array.length-1; i+=ip) {
+        // console.log(`instruction: ${array[i]}`)
+        // console.log(array)
+                //  A B C D E
+        let mode = ["1","0","0","0","0"]
+        //Parse the mode 
+        let modeString = array[i].toString().split('')
+
+        for (j=0; j<modeString.length; j++) {
+            mode[4-j] = modeString[(modeString.length-1)-j]
+        }
+        console.log(`Instruction: ${mode}`)
+        
+        //Set instruction pointer and paramaters
+        let parameters = []
+
+        switch (+mode[4]) {
+            case 3:
+                ip = 2 
+                userInput = prompt('userInput: ');
+                parameters.push({value: array[i+1], mode: 1})
+                break
+            case 4: 
+                ip = 2
+                parameters.push({value: array[i+1], mode: 1})
+                break
+            default: 
+                ip = 4
+                parameters.push({value: array[i+1], mode: mode[2]})
+                parameters.push({value: array[i+2], mode: mode[1]})
+                parameters.push({value: array[i+3], mode: mode[0]})
+        }
+        // console.log(`Parameters ${parameters[0].value}`)
+
+        let values = []
+        
+        parameters.forEach(param => {
+            if (param.mode == 1) {
+                values.push(param.value)
+            } else {
+                values.push(array[param.value])
+            }
+        })
+
+        switch(+mode[4]) {
+            case 1: 
+                array[values[2]] = values[0] + values[1]
+                break
+            case 2: 
+                array[values[2]] = values[0] * values[1]
+                break
+            case 3: 
+                console.log(`Saving ${userInput} to ${values[0]}`)
+                array[values[0]] = +userInput
+                break
+            case 4: 
+                console.log(values[0])
+                console.log(`index: ${i}`)
+                break
+        }
+        if (array[i + ip] == 99) {
+            // return array
+            // console.log(array)
+            console.log("============================== \n Program Finish")
+            return
         }
     }
 }
 
-IntcodeComputer(number_input)
+// let output = IntcodeComputer(["1101", "100", "-1", "4", "0"])
+let output = IntcodeComputer(string_input)
+// console.log(output)
