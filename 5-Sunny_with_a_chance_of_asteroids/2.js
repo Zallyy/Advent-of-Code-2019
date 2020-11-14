623c0f081b2d777c14aff6ae368dedd3ae95e0dc
Zallyy <Zallybags@gmail.com>
2020-11-14 09:27:04 -0800

    Day 5 part 2 progress

--- 5-Sunny_with_a_chance_of_asteroids/2.js
+++ 5-Sunny_with_a_chance_of_asteroids/2.js
@@ -6,34 +6,37 @@ const string_input = input.split(",").map(num => num) //Converts input to array 
 
 const IntcodeComputer = (program) => {
     const array = program.map(num => +num)
-    let userInput = 1
-    let ip //Instruction pointer 
-
-    for (let i=0; i<array.length-1; i+=ip) {
-        // console.log(array)
-        //           A   B   C   D   E
+    let systemID = 5
+   
+    let ip = 0
+    while (array[ip] != 99) {
         let mode = ["1","0","0","0","0"]
-        //Parse the mode 
-        let modeString = array[i].toString().split('')
-        console.log(modeString)
+        let modeString = array[ip].toString().split('')
+
         for (j=0; j<modeString.length; j++) {
             mode[4-j] = modeString[(modeString.length-1)-j]
         }
         console.log(`Instruction: ${mode}`)
-        
         let parameters = []
         switch (+mode[4]) {
+            case 1: 
+            case 2:
+            case 7:
+            case 8:
+                parameters.push({value: array[ip+1], mode: mode[2]})
+                parameters.push({value: array[ip+2], mode: mode[1]})
+                parameters.push({value: array[ip+3], mode: mode[0]})
+                break
             case 3:
             case 4: 
-                ip = 2 
-                parameters.push({value: array[i+1], mode: 1})
+                parameters.push({value: array[ip+1], mode: 1})
                 break
-            default: 
-                ip = 4
-                parameters.push({value: array[i+1], mode: mode[2]})
-                parameters.push({value: array[i+2], mode: mode[1]})
-                parameters.push({value: array[i+3], mode: mode[0]})
+            case 5: 
+            case 6:
+                parameters.push({value: array[ip+1], mode: mode[2]})
+                parameters.push({value: array[ip+2], mode: mode[1]})
         }
+
         let values = []
         parameters.forEach(param => {
             if (param.mode == 1) {
@@ -45,23 +48,54 @@ const IntcodeComputer = (program) => {
 
         switch(+mode[4]) {
             case 1: 
+                ip += 4
                 array[values[2]] = values[0] + values[1]
                 break
             case 2: 
+                ip += 4
                 array[values[2]] = values[0] * values[1]
                 break
             case 3: 
-                array[values[0]] = +userInput
+                ip += 2  
+                array[values[0]] = systemID
                 break
             case 4: 
+                ip += 2 
                 console.log(array[values[0]])
                 break
-        }
-        if (array[i + ip] == 99) {
-            console.log("============================== \n Program Finish \n============================== \n")
-            return
+            case 5: 
+                if (values[0] != 0) {
+                    ip = values[1]
+                } else {
+                    ip += 3
+                }
+                break
+            case 6: 
+                if (values[0] == 0) {
+                    ip = values[1]
+                } else {
+                    ip += 3
+                }
+                break
+            case 7:
+                ip += 4 
+                if (values[0] < values[1]) {
+                    values[2] = 1
+                } else {
+                    values[2] = 0
+                }
+                break
+            case 8: 
+                ip += 4
+                if (values[0] == values[1]) {
+                    values[2] = 1
+                } else {
+                    values[2] = 0
+                }
+                break
         }
     }
+    console.log("============================== \n Program Finish \n============================== \n")
 }
 
 //Test cases
@@ -70,3 +104,4 @@ const IntcodeComputer = (program) => {
 
 //Main case
 IntcodeComputer(string_input)
+// IntcodeComputer([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
