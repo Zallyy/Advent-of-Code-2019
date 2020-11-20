04247075393474f1a035a46bf103db545ba43441
Zallyy <Zallybags@gmail.com>
2020-11-19 16:42:38 -0800

    refactoring file read

--- 1-TheTyrannay_Of_The_Rocket_Equation/2.js
+++ 1-TheTyrannay_Of_The_Rocket_Equation/2.js
@@ -1,17 +1,8 @@
 // --- Day 1: The Tyranny of the Rocket Equation ---
-
 const fs = require('fs')
 
-fs.readFile('./input.txt', 'UTF-8', (err, data) => {
-    if (err) return console.log(err)
-    else {
-        const modules = data.split("\n")
-        modules.forEach( mass => {
-            CalculateFuel(mass)
-        })
-        console.log(`Total Fuel: ${totalFuel}`)
-    }
-})
+const data = fs.readFileSync('./input.txt', 'UTF-8')
+const modules = data.split("\n")
 
 let totalFuel = 0
 const CalculateFuel = (mass) => {
@@ -22,3 +13,8 @@ const CalculateFuel = (mass) => {
     totalFuel += fuelNeeded
     return CalculateFuel(fuelNeeded)
 }
+
+modules.forEach( mass => {
+    CalculateFuel(mass)
+})
+console.log(`Total Fuel: ${totalFuel}`)
